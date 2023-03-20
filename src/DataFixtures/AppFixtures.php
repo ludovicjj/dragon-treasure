c68ce0c0ee15582251db8b4ac918a3c02dafa52b
ludovicjj <jahanlud@gmail.com>
2023-03-20 15:23:30 +0100

    User fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\DragonTreasureFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,7 +12,6 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         DragonTreasureFactory::createMany(40);
-
-        $manager->flush();
+        UserFactory::createMany(10);
     }
 }
