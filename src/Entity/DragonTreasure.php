2cfca6507df7197bffab3f7cf473bfdf7282d580
ludovicjj <jahanlud@gmail.com>
2023-03-21 12:03:41 +0100

    DragonTreasure: embeded relation for item get operation (user.username) else use IRI

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -27,7 +27,11 @@ use Symfony\Component\Validator\Constraints as Assert;
     shortName: 'Treasure',
     description: 'A rare and valuable treasure.',
     operations: [
-        new Get(),
+        new Get(
+            normalizationContext: [
+                'groups' => ['treasure:read', 'treasure:item:get']
+            ]
+        ),
         new GetCollection(),
         new Post(),
         new Put(),
