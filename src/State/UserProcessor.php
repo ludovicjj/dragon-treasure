9ff4a8b164a7e8a01e85ef90a33c98f30acc8164
ludovicjj <jahanlud@gmail.com>
2023-03-24 18:00:48 +0100

    UserProcessor: hash plainPassword and defined password

---
+++ src/State/UserProcessor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private ProcessorInterface $persistProcessor,
+        private UserPasswordHasherInterface $passwordHasher
+    )
+    {
+    }
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        if ($data->getPlainPassword()) {
+            $data->setPassword($this->passwordHasher->hashPassword($data, $data->getPlainPassword()));
+        }
+        $data->eraseCredentials();
+
+        return $this->persistProcessor->process($data, $operation, $uriVariables, $context);
+    }
+}
