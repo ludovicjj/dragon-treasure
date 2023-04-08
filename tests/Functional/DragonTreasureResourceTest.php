f61b298e3f7f721b390be94c3b146c44345da050
ludovicjj <jahanlud@gmail.com>
2023-04-08 14:48:07 +0200

    Test: create treasure

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -3,8 +3,8 @@
 namespace App\Tests\Functional;
 
 use App\Factory\DragonTreasureFactory;
+use App\Factory\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Zenstruck\Browser\Json;
 use Zenstruck\Browser\Test\HasBrowser;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
@@ -41,5 +41,27 @@ class DragonTreasureResourceTest extends KernelTestCase
             ['@id', '@type', 'name', 'description', 'value', 'coolFactor', 'owner', 'shortDescription', 'plunderedAtAgo'],
             array_keys($json->decoded()['hydra:member'][0])
         );
+    }
+
+    public function testPostToCreateTreasure(): void
+    {
+        $user = UserFactory::createOne(['password' => 'pass']);
+        $this->browser()
+            ->actingAs($user)
+            ->post('/api/treasures', [
+                'json' => []
+            ])
+            ->assertStatus(422)
+            ->post('/api/treasures', [
+                'json' => [
+                    'name' => 'A shiny thing',
+                    'description' => 'It sparkles when I wave it in the air.',
+                    'value' => 1000,
+                    'coolFactor' => 5,
+                    'owner' => '/api/users/'.$user->getId(),
+                ]
+            ])
+            ->assertStatus(201)
+            ->assertJsonMatches('name', 'A shiny thing');
     }
 }
